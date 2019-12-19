cb377b27367f9d9b49178a0a4f5145229945150e
carolinecoding <cmeyer24@nscds.org>
2019-12-19 17:13:55 +0000

    added ability to click change mouth and eyes

--- emojicoloring/coloring.js
+++ emojicoloring/coloring.js
@@ -1,12 +1,27 @@
 $(".pupilImage").click(
     function(){
-        alert("I wish it was Friday");
+        //alert("I wish it was Friday");
+        var pupilClicked = $(this).css("background-image");
+        console.log(pupilClicked);
+        $(".pupil").css("background-image",pupilClicked);
     }
 
 );
 $(".mouthImage").click(
     function(){
-        alert("Ugh");
+        //alert("Ugh");
+         var mouthClicked = $(this).css("background-image");
+        console.log(mouthClicked);
+        $("#mouth").css("background-image",mouthClicked);
+    }
+
+);
+$(".color").click(
+    function(){
+        //alert("Ugh");
+         var colorClicked = $(this).css("background-image");
+        console.log(colorClicked);
+        $(".color").css("background-image",colorClicked);
     }
 
 );
