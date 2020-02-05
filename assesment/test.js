3f965a22b2c992891e8c8db0c90cc07f7b8bad7b
carolinecoding <cmeyer24@nscds.org>
2020-02-05 17:20:40 +0000

    assessment

---
+++ assesment/test.js
@@ -0,0 +1,10 @@
+$(".pupilImage").click(
+    function(){
+        alert("Hello")
+    }
+);
+$(".pupilImage").click(
+    function(){
+        var clickedThing = $(this).css("background-image")
+    }
+);
